62a0c720faf7f2d4dd9ab3f056aa7576a2c41a02
CalmDownKarm <karmanya.aggarwal.14cse@bml.edu.in>
2017-09-29 17:12:36 +0530

    updated assignment

--- AI/ai_assign.py
+++ AI/ai_assign.py
@@ -27,14 +27,14 @@ def dfs(start_node, goal_node, max_nodes):
                 global found
                 found = True
                 print("\n Found")
-                return visited
+                return (visited,stack)
             else:
                 if gen_left_child(top,max_nodes) >-1:
                     stack.append(gen_left_child(top,max_nodes))
                 if gen_right_child(top,max_nodes) >-1:
                     stack.append(gen_right_child(top,max_nodes))
     print("\n Not Found")
-    return visited    
+    return (visited,stack)    
 
 def bfs(start_node,goal_node,max_nodes):
     """Simple BFS, again depth limited"""
@@ -52,6 +52,8 @@ def bfs(start_node,goal_node,max_nodes):
             nodes_gen +=1
             visited.append(top)
             if goal_node == top:
+                print(len(visited))
+                print(len(queue))
                 return visited
             else:
                 if gen_left_child(top,max_nodes) >-1:
@@ -60,19 +62,40 @@ def bfs(start_node,goal_node,max_nodes):
                     queue.append(gen_right_child(top,max_nodes))
     
     print("\n Not Found")
-    return visited
+    
+    return (visited,queue)
 
 def depth_limited_search(start_node,goal_node):
     """You never actually need to implement this function, just call dfs with different values of max nodes"""
     # Since we have an implicit tree, regular dfs will never terminate. 
+    dlsvisited = dfs(start_node,goal_node,(2**3)-1)[0]
+    dlsstack = dfs(start_node,goal_node,(2**3)-1)[1]
+    print(dlsvisited)
+    print("\n Visited: "+repr(len(dlsvisited)))
+    print("Total Generated including visisted"+repr(len(dlsstack)+len(dlsvisited))) 
     return dfs(start_node,goal_node,(2**3)-1)
 
 def iterative_deepening_search(start_node,goal_node):
     """Just call on all values of max_node"""
-    return [dfs(start_node,goal_node,(2**max_node)-1)for max_node in range(1,15) if not found]
-
+    global found
+    found = False
+    visitedstack = [dfs(start_node,goal_node,(2**max_node)-1)[0] for max_node in range(1,15) if not found]
+    stacktotals = [dfs(start_node,goal_node,(2**max_node)-1)[1] for max_node in range(1,15) if not found]
+    print(visitedstack)
+    print(stacktotals)
+    totalvisited = sum([len(x) for x in visitedstack])
+    totalgenerated = sum([len(x) for x in stacktotals])+totalvisited
+    print("\n Visited : "+repr(totalvisited))
+    print("\n Total Generated including visited" + repr(totalgenerated))
+    return visitedstack
+# print("DFS")
 # print(dfs(1,7,15))
+# print("BFS")
 # print(bfs(1,10,15))
-# print(depth_limited_search(1,15))
+print("DLS")
+depth_limited_search(1,6)
+# print(depth_limited_search(1,6))
 import pprint
-pprint.pprint(iterative_deepening_search(1,7))
+print("IDS")
+iterative_deepening_search(1,7)
+# pprint.pprint())
